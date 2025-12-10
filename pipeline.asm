59b2a534df765731b5e94996a329ad18a5dfa0eb
Ross <rossarcher42@gmail.com>
2025-12-10 11:23:44 -0800

    Formatting changes, renaming changes

--- pipeline.asm
+++ pipeline.asm
@@ -17,25 +17,25 @@
 ; 00:0000 - 00:6FFF		-	Development area*
 
 
-BS		= $08
-LF		= $0A
-CR		= $0D
-DLE		= $10
+BS		= 	$08
+LF		= 	$0A
+CR		= 	$0D
+DLE		= 	$10
 CTRL_P  = DLE
-SP		= $20
-DEL		= $7F
+SP		= 	$20
+DEL		= 	$7F
 ; Frame format characters
-ST_X	= $02
-CTRL_B	= ST_X
-ETX		= $03
-CTRL_C	= ETX
-SOF		= ST_X
-EOF		= ETX
-ACK		= $06
-NAK		= $15
+ST_X		= 	$02
+CTRL_B		= 	ST_X
+ETX		= 	$03
+CTRL_C		= 	ETX
+SOF		= 	ST_X
+EOF		= 	ETX
+ACK		= 	$06
+NAK		= 	$15
 
 
-* 		= $40					; Zero page assignments
+* 		= 	$40					; Zero page assignments
 ;
 
 ;
@@ -46,45 +46,51 @@ CMD_ERROR
 CMD_IX		
 		.word	?				; Nexxt char in CMD_BUF  @ *(CMD_BUF + CMD_IX)
 
-EA_L	.byte	?				; 24 bit read pointer LOW
-EA_H	.byte	?				; 	" HIGH
-EA_B	.byte	?				; 	" PAGE #
-EA_PTR	=	EA_L				; Address of EA_PTR
+EA_L		.byte	?				; 24 bit read pointer LOW
+EA_H		.byte	?				; 	" HIGH
+EA_B		.byte	?				; 	" PAGE #
+EA_PTR		=	EA_L				; Address of EA_PTR
 
-CNT_L	.byte	?				; Must be 16 bits to transfer 16 bit index register
-CNT_H	.byte	?
-CNT		= CNT_L
+CNT_L		.byte	?				; Must be 16 bits to transfer 16 bit index register
+CNT_H		.byte	?
+CNT		= 	CNT_L
 
-TEMP	.byte	?
+TEMP		.byte	?
 
 
-SIZE_CMD_BUF	= 768			; maximum command length - pathological worst case for 256 payload bytes+
-*		= $0400					; CMD buffer
+SIZE_CMD_BUF	= 	768			; maximum command length - pathological worst case for 256 payload bytes+
+*		= 	$0400					; CMD buffer
 CMD_BUF		
 		.fill	SIZE_CMD_BUF
 
 * 		= $7E00
 ; Mirror the user state upon BRK exit state
 ; Note: do not re-order the next 16 bytes
-M_EFLAG	
-			.byte	?				; Track the mode we came from re-entering the monitor	
-M_STATE		=	M_EFLAG
+M_EMODE	
+		.byte	?				; Track the mode: (formerly called M_EFLAG but changed
+								; to multi-state to remember M and X flags)
+								; $FF = Emulation mode (65c816 = 65c02 more-or-less)
+								; $00 = Native mode, M=0 X=0 16 bit A,M, 16 bit X,Y
+								; $01 = Native mode, M=0 X=1 16 bit A,M, 8 bits X,Y
+								; $02 = Native mode, M=1 X=0 8 bits A,M, 16 bits X,Y
+								; $03 = Native mode, M=1 X=1 8 bits A,M, 8 bits X,Y
+M_STATE		=	M_EMODE 
 M_FLAGS 				
-			.byte	?				; 8 bits
-M_A			.byte	?				; lower 8 bits of A
-M_B			.byte	?				; B/upper 8 bits of A
-M_X			.word	?				; Always save as 16 bits
-M_Y			.word 	?				; Always save as 16 bits
+		.byte	?				; 8 bits
+M_A		.byte	?				; lower 8 bits of A
+M_B		.byte	?				; B/upper 8 bits of A
+M_X		.word	?				; Always save as 16 bits
+M_Y		.word 	?				; Always save as 16 bits
 M_SP		.word	?				; Always save as 16 bits
 M_DPR		.word	?			; Always 16 bits
 M_PC		.word	?
 M_PBR		.byte	?			; Always 8 bits
-M_DBR       .byte   ?                       ; Always 8 bits
+M_DBR       	.byte   ?                       ; Always 8 bits
 
 
 
 
-STACKTOP	= $7CFF				; Top of RAM (I/O 0x7F00-0x7FFF)
+STACKTOP	= 	$7CFF				; Top of RAM (I/O 0x7F00-0x7FFF)
 
 
 
@@ -152,7 +158,7 @@ CMD_PC1
 		LDA	CMD_STATE		; get state
 		ASL	A				; two bytes per entry
 		TAX					; 16 bit table offset (B|A)->X
-        JMP	(CMD_TBL,X)		; execute the current state
+        	JMP	(CMD_TBL,X)		; execute the current state
 		; No RTS - that happens in each finite state 
 
 
@@ -180,11 +186,11 @@ CMD_STATE_COLLECT
 		JSR	GET_FRAW
 		BCC	CMD_CX1				; if nothing in FIFO, quit
 		CMP	#EOF				; In case we hit the end of text
-		BNE CMD_CC2	
+		BNE	CMD_CC2	
 		LDA	#4					; Go process the command
 		STA	CMD_STATE
 		BRA	CMD_CX1				; 
-CMD_CC2	CMP	#DLE
+CMD_CC2		CMP	#DLE
 		BNE	CMD_CC3
 		LDA	#3
 		STA	CMD_STATE
@@ -201,20 +207,24 @@ CMD_CX1
 CMD_STATE_TRANSLATE
 		JSR	GET_FRAW
 		BCC	CMD_TX1				; If nothing in FIFO, quit 
-CMD_TC1	CMP	#EOF
+CMD_TC1		
+		CMP	#EOF
 		BNE	CMD_TC2
 		LDA	#4
 		STA	CMD_STATE
 		BRA	CMD_TX1
-CMD_TC2	CMP	#$11				; DLE' escaped SOF
+CMD_TC2		
+		CMP	#$11				; DLE' escaped SOF
 		BNE	CMD_TC3
 		LDA	#SOF
 		BRA	CMD_TXLAT
-CMD_TC3	CMP	#$12
+CMD_TC3		
+		CMP	#$12
 		BNE	CMD_TC4
 		LDA	#DLE
 		BRA	CMD_TXLAT
-CMD_TC4	CMP	#$13
+CMD_TC4		
+		CMP	#$13
 		BNE	CMD_TC5
 		LDA	#EOF
 		BRA	CMD_TXLAT
@@ -240,22 +250,28 @@ CMD_STATE_PROCESS
 		CMP	#1
 		BNE	PCBC1
 		JMP	RD_CMD
-PCBC1	CMP	#2
+PCBC1		
+		CMP	#2
 		BNE	PCBC2
 		JMP	WR_CMD
-PCBC2	CMP	#3
+PCBC2	
+		CMP	#3
 		BNE	PCBC3
 		JMP	GO_CMD
-PCBC3	CMP	#4					; set (volatile) Breakpoint
+PCBC3	
+		CMP	#4					; set (volatile) Breakpoint
 		BNE	PCBC4
 		JMP	SET_BP_CMD			; Breakpoint command	
-PCBC4	CMP	#5					; Get registers
+PCBC4	
+		CMP	#5					; Get registers
 		BNE	PCBC5
 		JMP	GET_CONTEXT
-PCBC5	CMP	#'E'				; echo command
+PCBC5	
+		CMP	#'E'				; echo command
 		BNE	PCBERR
 		JMP	ECHO_CMD
-PCBERR	JSR	SEND_NAK			; Unknown cmd
+PCBERR	
+		JSR	SEND_NAK			; Unknown cmd
 		RTS
 
 
@@ -276,12 +292,14 @@ RD_CMD
 		LDA	#SOF
 		JSR	PUTCH			; Unencoded SOF starts frame
 		LDY	#$FFFF			; Start at -1 because we send n+1 bytes :D
-RD_BN1	INY
+RD_BN1	
+		INY
 		LDA	[EA_PTR],Y		; Get next byte
 		JSR	CHR_ENCODE		; Send the byte there, possibly DLE escaped as two bytes
 		CPY	CNT				; Length word
 		BNE	RD_BN1
-RD_BX1	LDA	#EOF
+RD_BX1	
+		LDA	#EOF
 		JSR	PUTCH			; Unencoded EOF ends frame
 		RTS
 		
@@ -301,7 +319,8 @@ WR_CMD
 		STA	CNT_H
 		LDX	#4				; Index of first CMD_BUF byte to write
 		LDY	#0				; Where to write
-WR_BN1	LDA	CMD_BUF,X		; Get the next buffer byte
+WR_BN1	
+		LDA	CMD_BUF,X		; Get the next buffer byte
 		STA	[EA_PTR],Y		; Write it out
 		INX
 		INY
@@ -320,7 +339,7 @@ GO_CMD
 		LDA	CMD_BUF+3
 		STA	EA_B
 		JSR	SEND_ACK
-		JML [EA_PTR]
+		JML 	[EA_PTR]
 		
 ; [04][start-address-low][start-address-high][start-address-high]
 ; returns: replaced byte value (caller is responsible for replacing it!)	
@@ -339,25 +358,25 @@ SET_BP_CMD
 		LDA	#EOF
 		JSR	PUTCH
 		LDA	#$00			; BRK instruction (can't STZ indirect long)
-		STA [EA_PTR]		; Save a BRK there
+		STA 	[EA_PTR]		; Save a BRK there
 		RTS
 
 ; [05] GETCONTEXT
 ; Returns saved registers in block order 
 ; Returns [E-flag][Flags][A][B][XL][XH][YL][YH][SPL][SPH][DPRL][DPRH][PCL][PCH][PBR][DBR]
 GET_CONTEXT
-	LDA	#SOF
-	JSR	PUTCH
-	LDX	#0
+		LDA	#SOF
+		JSR	PUTCH
+		LDX	#0
 SSNC1
-	LDA	M_STATE,X
-	JSR	CHR_ENCODE
-	INX
-	CPX	#16
-	BNE	SSNC1
-	LDA	#EOF
-	JSR	PUTCH
-	RTS
+		LDA	M_STATE,X
+		JSR	CHR_ENCODE
+		INX
+		CPX	#16
+		BNE	SSNC1
+		LDA	#EOF
+		JSR	PUTCH
+		RTS
 		
 		
 
@@ -367,7 +386,8 @@ ECHO_CMD
 		LDA	#SOF
 		JSR	PUTCH
 		LDX	#0
-EC_CM1	LDA	CMD_BUF,X
+EC_CM1	
+		LDA	CMD_BUF,X
 		JSR	CHR_ENCODE		; Send byte, possibly DLE'ing it for transmission
 		INX
 		CPX	CMD_IX
@@ -377,18 +397,15 @@ EC_CM1	LDA	CMD_BUF,X
 		RTS
 
 
-
-	
-
 ;;;; ============================= New FIFO functions ======================================
 ; Initializes the system VIA (the USB debugger), and syncs with the USB chip.
 
-FIFO_TXE 	= PB0
-FIFO_RXF	= PB1
-FIFO_WR 	= PB2
-FIFO_RD 	= PB3
-FIFO_PWREN 	= PB5
-FIFO_DEBUG 	= PB7					; Handy debug toggle 
+FIFO_TXE 	= 	PB0
+FIFO_RXF	= 	PB1
+FIFO_WR 	= 	PB2
+FIFO_RD 	= 	PB3
+FIFO_PWREN 	= 	PB5
+FIFO_DEBUG 	= 	PB7					; Handy debug toggle 
 
 
 INIT_FIFO
@@ -469,16 +486,14 @@ SEND_NAK
 SEND_ACK
 		LDA	#ACK
 		STA	TEMP
-SENDC1	LDA	#SOF
+SENDC1	
+		LDA	#SOF
 		JSR	PUTCH
 		LDA	TEMP
 		JSR	PUTCH
 		LDA	#EOF
 		BRA	PUTCH
 		
-
-	
-
 ; This subroutine translates SOF DLE and EOF for inside-packet protection of OOB characters.  Enter at PUTCH by itself for untranslated output	
 CHR_ENCODE
 		CMP	#SOF
@@ -487,18 +502,20 @@ CHR_ENCODE
 		JSR	PUTCH
 		LDA	#$11
 		BRA	PUTCH
-WENC1	CMP	#DLE
+WENC1	
+		CMP	#DLE
 		BNE	WENC2
 		LDA	#DLE
 		JSR	PUTCH
 		LDA	#$12
 		BRA	PUTCH
-WENC2	CMP	#EOF
+WENC2	
+		CMP	#EOF
 		BNE	PUTCH
 		LDA	#DLE
 		JSR	PUTCH
 		LDA	#$13
-PUTCH	; Blocking char output
+PUTCH		; Blocking char output
 		JSR	PUT_FRAW
 		BCC	PUTCH
 		RTS
@@ -513,9 +530,10 @@ PUTSXL1
 PUTSY1 		
 		RTS 
 
-; Native BREAK exception handler:  Came from native mode user program!
-; return from user code (RAM) to monitor (ROM)
-BRK_NAT_ISR
+; Native BREAK exception handler:  Save full machine context 
+; and return to monitor with machine context.
+; Also, remember M and X flag settings and native mode 
+NAT_SAV_CONTEXT
 		SEP	#M_FLAG			; 
 		.as
 		STA	M_A
@@ -523,12 +541,12 @@ BRK_NAT_ISR
 		STA	M_B
 		PLA
 		STA	M_FLAGS			; Pull flags put on stack by BRK instruction
-		AND #$30			; Figure which E mode
+		AND 	#$30			; Figure which E mode
 		LSR	A
 		LSR	A
 		LSR	A
 		LSR	A
-		STA	M_EFLAG			; Remember the flag combinations		
+		STA	M_EMODE			; Remember the flag combinations		
 BNICP1
 		REP	#X_FLAG			; 16 bit index, binary mode
 		.xl
@@ -550,7 +568,8 @@ BNICP1
 		PLA
 		STA	M_DBR
 		; Fake up the stack to return to system monitor
-BNRET	LDA	#0				; Monitor is in bank #0
+BNRET		
+		LDA	#0				; Monitor is in bank #0
 		PHA					; push PBR=0
 		LDX	#START
 		PHX					; push "return address"
@@ -565,53 +584,52 @@ IRQ_ISR
 		
 		
 		
-	.xs
-	.as
 ; Emulated BREAK exception handler:  Came from emulation mode user program!
 ; return from user code (RAM) to monitor (ROM)
 ;    
-; Note:  M_EFLAG tells register dump which registers should be ignored
+; Note:  M_EMODE captures emulation mode and data size for the 5 mode combinations (0-3 or $FF)
 ;
-; FIXME:  MUST do a check for BRK flag on stack and divert to IRQ emulation handler before
-; using IRQs, e.g.
 ;
 BRK_IRQ_EMU
-		STA	M_A				; Save A and B
+		.xs
+		.as
+		STA	M_A			; Save A and B
 		XBA
 		STA	M_B
-		LDA	#$FF
-		STA	M_EFLAG			; E=1; we came from Emulation mode
-		PLA					; get flags from stack
-		PHA					; and put them back!
+		LDA	#$FF			; We came from emulation mode.  Remember that
+		STA	M_EMODE			; E=1; we came from Emulation mode
+		PLA				; get flags from stack
+		PHA				; and put them back!
 		AND	#BRK_FLAG		; Check for BRK flag
-		BNE	BRK_CONT		; continue break handling
-		LDA	M_B				; restore A & B 
+		BNE	BRK_SAV_CONTEXT		; continue break handling
+		LDA	M_B			; restore A & B 
 		XBA
 		LDA	M_A
 		BRA	IRQ_EMU_ISR		; EMU mode IRQ handler
-BRK_CONT
-		PLA					; Get flags off stack 
+; Save the entire machine context, remember emulation mode setting, and return to monitor
+BRK_SAV_CONTEXT
+		PLA				; Get flags off stack 
 		STA	M_FLAGS			; Pull flags put on stack by BRK instruction
 		STX	M_X
 		STZ	M_X+1
 		STY	M_Y
 		STZ	M_Y+1
-		PLA					; Pull PC7..0
+		PLA				; Pull PC7..0
 		SEC
 		SBC	#$02			; Subtract 2 from low address
 		STA	M_PC			; store as low PC
-		STA EA_L
-		PLA					; Get high PC PLA sets N and Z but leaves C alone fortunately
+		STA 	EA_L
+		PLA				; Get high PC PLA sets N and Z but leaves C alone fortunately
 		SBC	#$00			; take care of any borrow from low PC
 		STA	M_PC+1			; 
-		STA EA_H
+		STA 	EA_H
 		STZ	M_PBR			; Probably garbage, but slightly possibly holding future context 
-		TSX					; Now we're pulled everything off stack - it's pre-BRK position
+		TSX				; Now we're pulled everything off stack - it's pre-BRK position
 		STX	M_SP
 		LDA	#$01
 		STA	M_SP+1
-		PHD					; save DPR (zero page pointer).  Again, precautionary context save, 
-		PLA					; probably not important to user process (unless maybe it switches to native 			
+		PHD				; save DPR (zero page pointer).  Again, precautionary context save, 
+		PLA				; probably not important to user process (unless maybe it switches to native 			
 		STA	M_DPR+1			; at some point when it resumes.
 		PLA
 		STA	M_DPR
@@ -624,19 +642,20 @@ BRK_CONT
 		PHA
 		PHP
 		RTI				
-		
+; Put IRQ handlers for emulation mode here		
 IRQ_EMU_ISR
+		.xs
+		.as
 		RTI
 		
-		.xl
-		.as
 
 ; Restore machine to user context
-RESTORE		
-		LDA	M_EFLAG				; emulation mode?
-		BNE	RESNE				; If 0, we're restoring to a native context
+RES_CONTEXT
+		.as
+		LDA	M_EMODE				; emulation mode?
+		BNE	RES_EMU				; If 0, we're restoring to a native context
 ; Restore machine to user native context
-RESNN
+RES_NATIVE
 		REP	#(X_FLAG | D_FLAG)
 		.xl
 		SEP	#(M_FLAG)
@@ -666,7 +685,7 @@ RESNN
 		PLD
 		RTI										; RTI(24) Must use RTI to restore flags & Full 24 bit PC.  No other way!(?)
 ; Restore machine to user emulation context
-RESNE	
+RES_EMU	
 		SEP	#(X_FLAG | M_FLAG)
 		.xs
 		.as
@@ -701,7 +720,7 @@ RESNE
 		PHA
 		; Restore A & B
 		LDA   M_B
-        XBA
+        	XBA
 		LDA	  M_A
 		PLB
 		PLD
@@ -714,7 +733,7 @@ NCOP
 		.word	START		; COP exception in native mode
 * = $FFE6
 NBRK	
-		.word	BRK_NAT_ISR		; BRK in native mode
+		.word	NAT_SAV_CONTEXT	; BRK: Save context (coming from native mode)
 * = $FFE8
 NABORT	
 		.word	START
